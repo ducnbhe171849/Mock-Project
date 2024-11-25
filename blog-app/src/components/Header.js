f3837d8539356f53979e07022b11f23e15fa1476
Đỗ Hoàng Long <97176057+dolong232003@users.noreply.github.com>
2024-11-25 23:23:57 +0700

    Update Header.js

--- blog-app/src/components/Header.js
+++ blog-app/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     MDBNavbar,
     MDBContainer,
@@ -9,42 +9,53 @@ import {
     MDBNavbarToggler,
     MDBNavbarBrand,
     MDBCollapse,
-} from"mdb-react-ui-kit"
+} from "mdb-react-ui-kit"
 const Header = () => {
+    const [show, setShow] = useState(false);
     return (
         <div>
-            <MDBNavbar expand='lg' light style={{backgroundColor:"#541b1b"}}>
-                <MDBContainer fluid>
+            <MDBNavbar expand='lg' light style={{ backgroundColor: "#541b1b" }}>
+                {/* <MDBContainer fluid>
                     <MDBNavbarBrand href="/">
-                        <img src="/imgage/logo.jpg" alt="logo" style={{height:"30px"}}/>
+                        <img src="/imgage/logo.jpg" alt="logo" style={{ height: "30px" }} />
                     </MDBNavbarBrand>
-                </MDBContainer>
+                </MDBContainer> */}
                 <MDBNavbarToggler
-                type="button"
-                data-target ="#navbarColor02"
-                aria-controls="navbarColor02"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-                style={{color:"#fff"}}
-                onClick={()=>setShow(!show)}
+                    type="button"
+                    data-target="#navbarColor02"
+                    aria-controls="navbarColor02"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                    style={{ color: "#fff" }}
+                    onClick={() => setShow(!show)}
                 >
-                    <MDBIcon icon ="bars" fas/>
+                    <MDBIcon icon="bars" fas />
                 </MDBNavbarToggler>
                 <MDBCollapse show={show} navbar>
                     <MDBNavbarNav className="me-auto md-2 mb-lg-0">
-                        <MDBNavbarItem className ="active">
-                            <MDBNavbarLink aria-current="page" href="/" style={{color:"#fff"}}>
+                        <MDBNavbarItem className="active">
+                            <MDBNavbarLink aria-current="page" href="/" style={{ color: "#fff" }}>
                                 Home
                             </MDBNavbarLink>
                         </MDBNavbarItem>
                         <MDBNavbarItem >
-                            <MDBNavbarLink href="/addBlog" style={{color:"#fff"}}>
+                            <MDBNavbarLink href="/addBlog" style={{ color: "#fff" }}>
                                 Add Blog
                             </MDBNavbarLink>
                         </MDBNavbarItem>
                         <MDBNavbarItem >
-                            <MDBNavbarLink href="/about" style={{color:"#fff"}}>
+                            <MDBNavbarLink href="/about" style={{ color: "#fff" }}>
                                 About
+                            </MDBNavbarLink>
+                        </MDBNavbarItem>
+                        <MDBNavbarItem >
+                            <MDBNavbarLink href="/login" style={{ color: "#fff" }}>
+                                login
+                            </MDBNavbarLink>
+                        </MDBNavbarItem>
+                        <MDBNavbarItem >
+                            <MDBNavbarLink href="/signin" style={{ color: "#fff" }}>
+                                signin
                             </MDBNavbarLink>
                         </MDBNavbarItem>
                     </MDBNavbarNav>
